153fab23387c502f93887ed35ac1e5686247e4f7
a_200408 <astridsanabria4@gmail.com>
2024-02-27 17:39:46 -0500

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,158 @@
+package frc.robot;
+
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+public final class Constants {
+    
+    public static final class AutoConstants { //TODO: The below constants are used in the example auto, and must be tuned to specific robot
+        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+    
+        public static final double kPXController = 1;
+        public static final double kPYController = 1;
+        public static final double kPThetaController = 1;
+    
+        /* Constraint for the motion profilied robot angle controller */
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+            new TrapezoidProfile.Constraints(
+         kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+
+//* arm constants
+            
+    }
+    public static final class ArmConstants {
+        public static final int kLeftID = 14;
+        public static final int kRightID = 15;
+        public static final double kStowPos = 0.5;
+    
+        //these 2 should be the same
+        public static final double kIntakePos = 0.06;
+        public static final double kSubwooferPos = 0.06;
+    
+        public static final double kFrontAmpPos = 0.15;
+    
+        // 3m position
+        public static final double k3mPos = 0.12;
+    
+        public static final double kBackAmpPos = 0.27;
+        public static final double kUpperLimit = 0.3;
+        public static final double kLowerLimit = 0.01;
+        public static final double kOverrunLimit = 0;
+    
+        public static final double kP = 1.5; 
+        public static final double kI = 0;
+        public static final double kD = 0; 
+        public static final double kIz = 0; 
+        public static final double kFF = 0; 
+        public static final double kMaxOutput = 0.7; 
+        public static final double kMinOutput = -0.7;
+        public static final double kMaxAccel = 0.18;
+        public static final double kMaxVel = 0.85;
+    
+        public static final double kTolearance = 0.002;
+        public static final double kManualSpeed = 0.3;
+
+//*  shooter constants
+
+      }
+      public static final class ShooterConstants {
+        public static final int kTopID = 8;
+        public static final int kBottomID = 9;
+    
+        // init v should be 6.7 m/s for subwoofer at intake pos
+        public static final double kSubwooferSpeed = 1259;
+    
+        // init v should be 8.2 m/s
+        public static final double k3mSpeed = 1541;
+    
+        public static final double kCompenstion = 1.13;
+    
+        public static final double kIdleSpeed = 0.3;
+        public static final double kAmpSpeed = 0.2;
+    
+        public static final double kP = 6e-5;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+        public static final double kFF = 0.000015;
+        public static final double kIz = 0;
+        public static final double kMinOutput = -1;
+        public static final double kMaxOutput = 1;
+        public static final double kMaxRPM = 5676;
+        public static final double kLaunchTime = 0.5;
+
+//* Climber constants 
+
+      }
+    public static final class ClimberConstants {
+    public static final int kPortID = 15;
+    public static final int kStarboardID = 16;
+    public static final int kPortDIO = 1;
+    public static final int kStarboardDIO = 2;
+    public static final double kP = 0.1; 
+    public static final double kI = 1e-4;
+    public static final double kD = 1; 
+    public static final double kIz = 0; 
+    public static final double kFF = 0; 
+    public static final double kMaxOutput = 1; 
+    public static final double kMinOutput = -1;
+
+    public static final double kHomeSpeed = -0.4;
+    
+    public static final double kUpperLimit = 10;
+    public static final double kLowerLimit = 0;
+    public static final double kManualSpeed = 0;
+}
+
+// Cannot remember if the Extreme 3D PRO is 0 index or not.
+public static final class DriverConstants {
+  public static final double kDefaultSpeed = 0.8;
+  public static final int kSetX = 5;
+  public static final int kIntake = 0;
+  public static final int kHoldArmDown = 6;
+  public static final int kAutoIntake = 3; 
+  public static final int kAutoAmp = 4;
+  public static final int kSelfDestruct = 9;
+  public static final int kTurbo = 1;
+
+//* intake constants
+
+}
+ public static final class IntakeConstants {
+            public static final int kIntakeID = 10;
+            public static final int kSensorDIOPort = 0;
+            public static final double kIntakeSpeed = 3098;
+            public static final double kFeedSpeed = 3083;
+            public static final double kReverseSpeed = 0.4;
+            public static final double kCompenstion = 1.83;
+        
+            public static final double kP = 6e-5;
+            public static final double kI = 0.0;
+            public static final double kD = 0.0;
+            public static final double kFF = 0.000015;
+            public static final double kIz = 0;
+            public static final double kMinOutput = -1;
+            public static final double kMaxOutput = 1;
+            public static final double kMaxRPM = 11000;    
+          }
+
+          public static final class OIConstants {
+            public static final int kDriverControllerPort = 0;
+            public static final double kDriveDeadband = 0.1;
+            public static final int kOperatorControllerPort = 1;
+             }
+
+             // operator constants
+
+            public static final class OperatorConstants {
+                public static final double kManoeuvreSpeed = 0.4; 
+            }
+    }
+
+  
---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,134 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.StrictFollower;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.mechanisms.swerve.utility.PhoenixPIDController;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CANcoderConfigurator;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+
+/*import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;*/
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
+
+public class Arm extends SubsystemBase{
+
+    /*We need methods to run the arm to positions using a rev throughbore encoder in absolute mode.
+     * It is needed to read arm position and to move the arm.
+     */
+    private final TalonFX left;
+    private final TalonFX right;
+    private final CANcoder encoder;
+    private final CurrentLimitsConfigs m_currentLimits = new CurrentLimitsConfigs();
+    private final DutyCycleOut m_output = new DutyCycleOut(0);
+    private final VelocityVoltage m_voltageVelocity = new VelocityVoltage(0, 0, true, 0, 0, false, false, false);
+    /*private final ArmFeedforward ff =
+        new ArmFeedforward(
+        ArmConstants.kSVolts, ArmConstants.kGVolts,
+        ArmConstants.kVVoltSecondPerRad, ArmConstants.kAVoltSecondSquaredPerRad);*/
+    public Arm (int leftID, int rightID) {
+        
+        left = new TalonFX(leftID);
+        right = new TalonFX(rightID);
+        TalonFXConfiguration TalonFXConfigs = new TalonFXConfiguration();
+        TalonFXConfigurator LeftTalonFXConfigurator = left.getConfigurator();
+        TalonFXConfigurator RightTalonFXConfigurator = right.getConfigurator();
+        // sets to factory default
+        LeftTalonFXConfigurator.apply(new TalonFXConfiguration());
+        RightTalonFXConfigurator.apply(new TalonFXConfiguration());
+       // creates current limit for motors
+        m_currentLimits.SupplyCurrentLimit = 40;
+        TalonFXConfigs.CurrentLimits = m_currentLimits;
+        //set current limit for left motor
+        LeftTalonFXConfigurator.apply(m_currentLimits); 
+        //set current limit for right motor
+        RightTalonFXConfigurator.apply(m_currentLimits); 
+        //inverts one motor to create rotation
+        left.setInverted(false);
+        right.setInverted(true);
+        //declares new cancoder
+        encoder = new CANcoder(21);
+        CANcoderConfiguration CANcoderConfigs = new CANcoderConfiguration();
+    /* User can change the configs if they want, or leave it empty for factory-default */
+        encoder.getConfigurator().apply(CANcoderConfigs);
+        /*  invert in software, 
+        encoder.setInverted; */
+       // strict followers ignore the leader's invert and use their own
+        right.setControl(new StrictFollower(left.getDeviceID()));
+        // creates PID values for motor (hopefully)
+        var slot0Configs = new Slot0Configs();
+        slot0Configs.kP = ArmConstants.kP;
+        slot0Configs.kI = ArmConstants.kI;
+        slot0Configs.kD = ArmConstants.kD;
+        left.getConfigurator().apply(slot0Configs);
+    }
+
+    public void runOpenLoop(double supplier) {
+        if(getPos() >= ArmConstants.kUpperLimit) {
+            left.set(0);;
+            System.out.println("¡TOO HIGH! ¡UPPER LIMIT!");
+        }
+        else if(getPos() <= ArmConstants.kLowerLimit) {
+            left.set(0);;
+            System.out.println("¡TOO LOW! ¡LOWER LIMIT!");
+        }
+        else {
+            left.set(supplier);
+        }
+    }
+
+   /*  public void hold(TrapezoidProfile.State setpoint) {
+        double feedforward = ff.calculate(setpoint.position*2*Math.PI, setpoint.velocity);
+        //figure out how to convert the below part to phoenix 6
+            left.setPosition(setpoint, 1);
+    } */
+
+    public void runToPosition(double setpoint) {
+        //these are included safety measures. not necessary, but useful
+       /*  if(getPos() >= ArmConstants.kUpperLimit) {
+            left.set(0);;
+            System.out.println("¡TOO HIGH! ¡UPPER LIMIT!");
+        }
+        else if(getPos() <= ArmConstants.kLowerLimit) {
+            left.set(0);;
+            System.out.println("¡TOO LOW! ¡LOWER LIMIT! " + getPos());
+        }
+        else{*/
+        //figure out how to convert the below part to phoenix 6
+        //left.setControl(m_voltageVelocity.withVelocity(setpoint));
+        left.set(setpoint);
+    }
+    
+
+    public double getPos() {
+        return encoder.getPosition().getValueAsDouble();
+    }
+    
+}
+
+
+
